2f84167647be97faea94b76bc2b5d7e89a63c492
Savwede <111609631+Savwede@users.noreply.github.com>
2022-11-30 02:45:45 +0100

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     result = ''
     for char in str:
-        if ord('a') <= ord(c) <= ord('z'):
+        if ord('a') <= ord(char) <= ord('z'):
             result += chr(ord(char) - 32)
     print("{}".format(result))
     return (result)
