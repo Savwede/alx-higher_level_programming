bce78088df89ac9415a51c07e3133921c5233386
Savwede <111609631+Savwede@users.noreply.github.com>
2022-11-30 03:53:21 +0100

    Update 10-add.py

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def add(a, b):
-    c = 1
-    if type(a) is type(c) and type(b) is type(c):
-        return (a + b)
-    else:
-        return
+    c = a + b
+    return (int(c))
