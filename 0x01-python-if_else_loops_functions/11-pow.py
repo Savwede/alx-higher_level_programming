ff81dd9d7620f521e4b66b90fefbe0e46c0f60d2
Savwede <111609631+Savwede@users.noreply.github.com>
2022-11-30 04:28:58 +0100

    Update 11-pow.py

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
 def pow(a, b):
-    c = math.pow(a, b)
-    return (int(c))
+    c = 1
+    d = 0
+    if b < 0:
+        d = 1
+        b *= -1
+    for i in range(b):
+        if b is not 0:
+            c *= a
+    if d is 1:
+        return (int(1 / c))
+    else:
+        return (int(c))
