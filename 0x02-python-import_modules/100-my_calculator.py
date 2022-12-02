0cb79a24416d60cd3d29dea5d6f71f46e7cac5dd
Savwede <111609631+Savwede@users.noreply.github.com>
2022-12-02 05:43:11 +0100

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,14 +1,33 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-	    """Print the number of and list of arguments."""
-		    import sys
+    """Print the number of and list of arguments."""
+    import sys
+    from calculator_1.py import add
+    from calculator_1.py import sub
+    from calculator_1.py import mul
+    from calculator_1.py import div
+    
+    count = len(sys.argv) - 1
+    if count != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+	exit(1)
+    else:
+        a = int(sys.argv[1])
+	b = int(sys.argv[3])
+	result = 0
+	opp = sys.argv[2]
+	opps = "+-*/"
+	if opp == "+":
+	    result = add(a, b)
+	elif opp == "-":
+	    result = sub(a, b)
+	elif opp == "*":
+	    result = mul(a, b)
+	elif opp == "/":
+	    result = div(a, b)
+	else:
+	    print("Unknown operator. Available operators: +, -, * and /")
+	    exit(1)
+	if opps.find(opp) != -1
+	    print("{} {} {} = {}".format(a, opp, b, result))
 
-			    count = len(sys.argv) - 1
-				    if count == 0:
-						        print("0 arguments.")
-					elif count == 1:
-						        print("1 argument:")
-					else:
-						        print("{} arguments:".format(count))
-								    for i in range(count):
-										        print("{}: {}".format(i + 1, sys.argv[i + 1]))
