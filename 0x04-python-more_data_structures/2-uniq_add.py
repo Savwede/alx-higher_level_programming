233c600479042c5491f403b25c9a0cb0367099ff
Savwede <111609631+Savwede@users.noreply.github.com>
2022-12-07 23:29:11 +0100

    Update 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -5,9 +5,7 @@ def uniq_add(my_list=[]):
     nlist = []
     sum = 0
     for i in my_list:
-        if(nlist.index(i)):
-            pass
-        else:
+        if(nlist.count(i) == 0):
             nlist.append(i)
             sum += i
     return sum
