1079d3ad3a8ed8ca283350e8068b91629c9f043a
Savwede <111609631+Savwede@users.noreply.github.com>
2022-12-07 23:06:50 +0100

    Update 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 
+
 def uniq_add(my_list=[]):
-    
+    nlist = []
+    sum = 0
+    for i in my_list:
+        if(!nlist.index(i)):
+            nlist.append(i)
+            sum += i
+    return sum
