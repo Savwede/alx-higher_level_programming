47fbe2da25717ef60ca89ec91b12a68449df275a
Savwede <111609631+Savwede@users.noreply.github.com>
2022-12-08 00:29:29 +0100

    Update 7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 
+
 def update_dictionary(a_dictionary, key, value):
+    for item in a_dictionary.keys():
+        if (item == key):
+            a_dictionary[item] = value
+        else:
+            a_dictionary.update({key: value})
