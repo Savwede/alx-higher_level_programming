5a7c6097b59475b46792f5641d46574fd3d652c1
Savwede <111609631+Savwede@users.noreply.github.com>
2022-12-08 01:04:26 +0100

    Update 8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 
+
 def simple_delete(a_dictionary, key=""):
+    for val in a_dictionary.keys():
+        if(val == key):
+            a_dictionary.pop(val)
+    return a_dictionary
