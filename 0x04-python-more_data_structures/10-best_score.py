b8e7bbdaed53a86bd6a2caa97e4cce82cf9ea854
Savwede <111609631+Savwede@users.noreply.github.com>
2022-12-08 01:23:36 +0100

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 
+
 def best_score(a_dictionary):
+    big = -100000
+    key = None
+    if(a_dictionary == None):
+        return key
+    for val in a_dictionary.keys():
+        if(a_dictionary[val] > big):
+            big = a_dictionary[val]
+            key = val
+    return key
